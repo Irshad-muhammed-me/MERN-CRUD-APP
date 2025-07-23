b53c0f5d8facb97320a52600c26bc24fb48acfff
Irshad-muhammed-me <irashimuhammed@gmail.com>
2025-07-23 15:21:32 +0530

    new

--- Client/src/Users.jsx
+++ Client/src/Users.jsx
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+
+const API_URL = process.env.REACT_APP_API_URL;
+
 const Users = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     axios
-      .get("http://localhost:3001")
+      .get(`${API_URL}/`)
       .then((result) => setUsers(result.data))
       .catch((err) => console.log(err));
   }, []);
 
   const handleDelete = (id) => {
     axios
-      .delete("http://localhost:3001/deleteUser/" + id)
+      .delete(`${API_URL}/deleteUser/` + id)
       .then((res) => {
         console.log(res);
         window.location.reload();
